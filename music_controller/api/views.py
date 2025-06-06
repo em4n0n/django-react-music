e4c0181e3a3ae2a16731e648c1af86284b9d1176
david <dmsim159@gmail.com>
2025-06-05 22:16:34 -0700

    Add an h1 tag

--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def main(request):
-    return HttpResponse("Welcome to the Music Controller API!")
+    return HttpResponse("<h1>Welcome to the Music Controller API!</h1>")
