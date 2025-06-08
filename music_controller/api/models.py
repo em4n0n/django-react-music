9ab9052236690d978b16d618a347569599f409cd
david <dmsim159@gmail.com>
2025-06-07 21:25:17 -0700

    Create a function to return a random code

--- music_controller/api/models.py
+++ music_controller/api/models.py
@@ -1,5 +1,13 @@
 from django.db import models
-
+import string
+import random
+def generate_unique_code():
+    length = 6
+    while True:
+        code = ''.join(random.choices(string.ascii_uppercase + string.digits, k=length))
+        if Room.objects.filter(code=code).count() == 0:
+            break
+    return code
 # Create your models here.
 class Room(models.Model):
     code = models.CharField(max_length=8, default= '', unique=True)
