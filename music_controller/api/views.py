f15655959a8617ced58ead459a4aa2716a7cd7e5
david <dmsim159@gmail.com>
2025-06-08 21:01:30 -0700

    Import Serializers and Room Model + Define the queryset and serializer class

--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -1,2 +1,7 @@
 from rest_framework import generics
+from .serializers import RoomSerializer
+from .models import Room
 
+class RoomView(generics.ListCreateAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
