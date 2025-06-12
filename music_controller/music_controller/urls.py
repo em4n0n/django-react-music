9ab4cdf495160e961a1e6959218071c74cc472be
david <dmsim159@gmail.com>
2025-06-12 15:52:05 -0700

    Route front.urls

--- music_controller/music_controller/urls.py
+++ music_controller/music_controller/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('api.urls')),  # Include the API URLs
+    path('', include('api.urls')),
+    path('', include('frontend.urls'))# Include the API URLs
 ]
